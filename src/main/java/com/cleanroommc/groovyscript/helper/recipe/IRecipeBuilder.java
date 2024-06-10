c8fb82eb8fcb5b9c229d5dd63f82ef3cc3f44cd0
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2024-06-09 17:14:10 -0700

    convert register from T into List<T, require Nonnull

--- src/main/java/com/cleanroommc/groovyscript/helper/recipe/IRecipeBuilder.java
+++ src/main/java/com/cleanroommc/groovyscript/helper/recipe/IRecipeBuilder.java
@@ -3,6 +3,11 @@ package com.cleanroommc.groovyscript.helper.recipe;
 import com.cleanroommc.groovyscript.api.documentation.annotations.RecipeBuilderRegistrationMethod;
 import org.jetbrains.annotations.Nullable;
 
+import javax.annotation.Nonnull;
+import java.util.List;
+
+import java.util.List;
+
 /**
  * A groovy recipe builder template
  *
@@ -19,11 +24,12 @@ public interface IRecipeBuilder<T> {
 
     /**
      * This method creates and registers a recipe using the values of this builder.
-     * Should call {@link #validate()} and return null if values are not valid.
+     * Should call {@link #validate()} and returns an empty list if the values are not valid.
+     * Should never return {@code null}.
      *
-     * @return the built recipe or null if values are invalid
+     * @return the built recipe or an empty list if values are invalid
      */
-    @Nullable
+    @Nonnull
     @RecipeBuilderRegistrationMethod
-    T register();
+    List<T> register();
 }

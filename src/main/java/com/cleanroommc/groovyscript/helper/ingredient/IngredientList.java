31f720bd9cc5ed80fadbb3f9ef6a3c34e9f9753b
Waiting Idly <25394029+WaitingIdly@users.noreply.github.com>
2025-10-10 00:59:17 -0700

    add getOrEmpty to IngredientList

--- src/main/java/com/cleanroommc/groovyscript/helper/ingredient/IngredientList.java
+++ src/main/java/com/cleanroommc/groovyscript/helper/ingredient/IngredientList.java
@@ -9,6 +9,14 @@ import java.util.ArrayList;
  */
 public class IngredientList<T extends IIngredient> extends ArrayList<T> {
 
+    public IIngredient getOrEmpty(int i) {
+        if (i < 0 || i >= size()) {
+            return IIngredient.EMPTY;
+        }
+        IIngredient item = get(i);
+        return item == null ? IIngredient.EMPTY : item;
+    }
+
     public int getRealSize() {
         if (isEmpty()) return 0;
         int realSize = 0;
